3d69de880dc2215abb4f4a8383d9c7724f419a49
能勢大輔 <nosedaisuke@nosedaisukenoMacBook-Air.local>
2019-01-13 18:42:38 +0900

    [finish]アプリケーション基礎

--- BOOKERS/app/controllers/books_controller.rb
+++ BOOKERS/app/controllers/books_controller.rb
@@ -17,15 +17,18 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
+  	flash[:notice] = "successfully"
   	redirect_to book_path(book.id)
   end
   def edit
   	@book = Book.find(params[:id])
+
   end
 
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
+  	flash[:notice] = "successfully"
   	redirect_to book_path(book.id)
   end
 
